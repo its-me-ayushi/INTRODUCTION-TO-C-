03f4538a88b15605aee2b4b531309673dedd159e
ayushi adya <105603159+its-me-ayushi@users.noreply.github.com>
2022-06-22 23:50:36 +0530

    Create pointer-output.cpp

---
+++ pointer-output.cpp
@@ -0,0 +1,16 @@
+ Pointer Declaration
+
+Which of the following is the proper declaration of a pointer?
+
+
+Options
+
+
+This problem may have one or more correct answers
+
+int x;
+int &x;
+int *x; 
+ptr x;
+
+Correct Answer =  int *x; 
